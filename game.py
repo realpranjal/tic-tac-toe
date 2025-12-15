8d5be923e285a8200d3eff770713b6259bfad0f6
realpranjal <130909499+realpranjal@users.noreply.github.com>
2025-12-15 16:24:09 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,10 @@
+import random
+
+Q = {}
+alpha, gamma, ep = 0.1, 0.9, 0.5
+
+def choose_action(state, actions):
+  if random.random() < eps:
+    return random.choices(actions)
+  return max(actions, key=lambda a: Q.get((state, a), 0)
 
